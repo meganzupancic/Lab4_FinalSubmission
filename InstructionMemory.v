e04ec80c6fb3bbb991b1e72a7263f200f7fe9b4a
meganzupancic <118412810+meganzupancic@users.noreply.github.com>
2024-10-29 17:53:54 -0700

    Update InstructionMemory.v

--- InstructionMemory.v
+++ InstructionMemory.v
@@ -54,7 +54,7 @@ module InstructionMemory(Address, Instruction);
     
         //$readmemh("C:/Users/megzu/OneDrive - University of Arizona/ECE 369/Lab 4/MIPS tests/addi_$t1_$zero_6.mem", memory);
         //$readmemh("C:/Users/meganzupancic/Downloads/test.mem", memory); 
-        $readmemh("C:/Users/athiel/Downloads/testv2.mem", memory);
+        $readmemh("test.mem", memory);
         
         
         $display("Memory Contents:");
