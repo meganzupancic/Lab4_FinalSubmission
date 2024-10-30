07c507cf7fc7a03a573599664f08390c9231294e
athiel1 <157844495+athiel1@users.noreply.github.com>
2024-10-29 17:56:14 -0700

    Update Top_Level.v

--- Top_Level.v
+++ Top_Level.v
@@ -1,21 +1,10 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 10/25/2024 09:25:15 PM
-// Design Name: 
-// Module Name: Top_Level
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
+//
+// ECE 369 - Lab 4
+//
+// Names: Megan Zupancic and Annie Thiel
+// Percent Effort: 50/50
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
